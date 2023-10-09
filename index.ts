ea2f05c11e53084844d5f94a34f7405b6a22c5bd
guithin <on14@naver.com>
2023-10-09 22:23:31 +0900

    auth

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 
 export type { ExampleIO } from './interfaces/example';
 export type { StorageIO } from './interfaces/storage';
+export type { AuthIO } from './interfaces/auth';
---
+++ interfaces/auth.ts
@@ -0,0 +1,16 @@
+import { ExTypes } from './base';
+
+interface LoginReqB {
+  userId: string;
+  password: string;
+}
+
+interface LoginResB {
+  token: string;
+  name: string;
+}
+
+// P, ResB, ReqB, ReqQ, Locs
+export declare namespace AuthIO {
+  type Login = ExTypes<{}, LoginResB, LoginReqB>;
+}
